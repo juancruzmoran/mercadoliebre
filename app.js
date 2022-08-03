7575b2a96ce1568e33b9b33851ccb7146c4a8788
juancruzmoran <juancruzmoran@outlook.es>
2022-08-03 18:35:57 -0300

    register

--- app.js
+++ app.js
@@ -7,5 +7,7 @@ const port = 3030;
 app.use(express.static('public'));
 //rutas
 app.get('/', (req,res) => res.sendFile(path.join(__dirname,'views','home.html')))
+app.get('/login', (req,res) => res.sendFile(path.join(__dirname,'views','login.html')))
+app.get('/register', (req,res) => res.sendFile(path.join(__dirname,'views','register.html')))
 //servidorAA
 app.listen(port, () => console.log('Server running in http://localhost:' + port));
